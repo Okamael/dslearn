4c5010ffc2cfebafb692e0728d9d529a7b792315
Murilo Ramos <okamael.adc@gmail.com>
2022-08-08 14:58:48 -0300

    add User findbyId

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +12,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslearnbds.dto.UserDTO;
 import com.devsuperior.dslearnbds.entities.User;
-import com.devsuperior.dslearnbds.repositories.RoleRepository;
 import com.devsuperior.dslearnbds.repositories.UserRepository;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
 @Service
 public class UserService implements UserDetailsService{
 	
@@ -18,6 +23,14 @@ public class UserService implements UserDetailsService{
 	private UserRepository repository;
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Transactional
+	public UserDTO findById(Long id) {
+		Optional<User> optional = repository.findById(id);
+		User entity = optional.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
+		return new UserDTO(entity);
+	}
 	
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User  user = repository.findByEmail(username);
